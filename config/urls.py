f6b7608885316df7a244b38348a76a0d31e758e0
이상혁 <lsh11946@naver.com>
2024-09-19 18:02:47 +0900

    DEBUG 404

--- config/urls.py
+++ config/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from cgitb import handler
+
 from django.contrib import admin
 from django.urls import include, path
 from pybo.views import base_views
@@ -24,3 +26,5 @@ urlpatterns = [
     path('common/', include('common.urls')),
     path('', base_views.index, name='index'),  # '/' 에 해당되는 path
 ]
+
+handler404 = 'common.views.page_not_found'
